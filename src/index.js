a72dbe76aa476bfb69cc3fd169d5af748ad5dfcf
jesussangho <jesussangho@gmail.com>
2020-08-21 10:24:53 +0900

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+const HelloWorld = () => <div>HELLO WORLD</div>;
+const OtherWorld = () => <div>OTHER WORLD</div>;
+const AnotherWorld = () => <div>ANOTHER WORLD</div>;
+
+const SuperWorld = () => (
+  <div>
+    <div>
+      <h1>easy peasy</h1>
+    </div>
+    <HelloWorld />
+    <OtherWorld />
+    <AnotherWorld />
+  </div>
+);
+
+ReactDOM.render(<SuperWorld />, document.getElementById("root")); // <함수 />, ~ html id
