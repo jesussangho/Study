54e036116e9384b1dcdbd8ebf3329c023491abe5
jesussangho <jesussangho@gmail.com>
2020-08-21 11:13:28 +0900

    컴포넌트간 데이터 이동

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const HelloWorld = () => <div>HELLO WORLD</div>;
-const OtherWorld = () => <div>OTHER WORLD</div>;
-const AnotherWorld = () => <div>ANOTHER WORLD</div>;
-
-const SuperWorld = () => (
+const TodoList = props => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
+const App = () => (
   <div>
-    <div>
-      <h1>easy peasy</h1>
-    </div>
-    <HelloWorld />
-    <OtherWorld />
-    <AnotherWorld />
+    <h2>My Todo List</h2>
+    <TodoList todos={["Learn React", "Crush Recast.ly", "Maybe sleep"]} />
   </div>
 );
 
-ReactDOM.render(<SuperWorld />, document.getElementById("root")); // <함수 />, ~ html id
+ReactDOM.render(<App />, document.getElementById("root"));
