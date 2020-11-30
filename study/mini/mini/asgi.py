35ed0ba6cd37f0448a01a4af965026d0577971c7
jesussangho <jesussangho@gmail.com>
2020-11-30 15:54:25 +0900

    polls

--- study/mini/mini/asgi.py
+++ study/mini/mini/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Devmap project.
+ASGI config for mini project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Devmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini.settings')
 
 application = get_asgi_application()
--- study/mini/mini/wsgi.py
+++ study/mini/mini/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Devmap project.
+WSGI config for mini project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Devmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini.settings')
 
 application = get_wsgi_application()
