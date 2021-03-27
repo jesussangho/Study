566e28d6fcbd4c02104ae7efd2019b7d5ac6ddcb
jesussangho <jesussangho@gmail.com>
2021-03-27 22:51:49 +0900

    fix Not Found

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 var http = require("http");
 var fs = require("fs");
-var url = require("url"); // require : 요구하다
+var url = require("url");
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    // nodejs url parse query string : nodejs url 쿼리 분석
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    console.log(queryData.id);
-    if (_url === "/") {
-        title = "Welcome";
+
+    if (pathname === "/") {
+        fs.readFile(
+            `data/${queryData.id}`,
+            "utf-8",
+            function (err, description) {
+                var template = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Sangho ${title}</title>
+            </head>
+            <body>
+                <h1><a href="/?id=welcome">Web Study</h1>
+                <ul>
+                    <li><a href="/?id=home">home</a></li>
+                    <li><a href="/?id=Portfolio">Portfolio</a></li>
+                    <li><a href="/?id=About">About</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${description}</p>
+            </body>
+            </html>
+        `;
+                response.writeHead(200);
+                response.end(template);
+            }
+        );
+    } else {
+        response.writeHead(404);
+        response.end("Not Found");
     }
-    if (_url === "/favicon.ico") {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    // console.log(__dirname + _url);
-    // response.end(fs.readFileSync(__dirname + _url));
-    // response.end("sangho : " + _url);
-    fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
-        var template = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sangho ${title}</title>
-        </head>
-        <body>
-            <h1><a href="/?id=welcome">Web Study</h1>
-            <ul>
-                <li><a href="/?id=home">home</a></li>
-                <li><a href="/?id=Portfolio">Portfolio</a></li>
-                <li><a href="/?id=About">About</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-        </body>
-        </html>
-    `;
-        response.end(template);
-    });
 });
 app.listen(3000);
