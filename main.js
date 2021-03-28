fba7ddfbf07e068ea69d56b2fe8bfdbeff6d04ad
jesussangho <jesussangho@gmail.com>
2021-03-28 19:43:05 +0900

    main.js-refactoring

--- main.js
+++ main.js
@@ -2,70 +2,75 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateHTML(title, list, body) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sangho ${title}</title>
+    </head>
+    <body>
+        <h1><a href="/">Web Study</h1>
+        ${list}
+        ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+    var list = "<ul>";
+    var i = 0;
+    while (i < filelist.length) {
+        list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
+    }
+    list = list + "</ul>";
+    return list;
+}
+
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     if (pathname === "/") {
         if (queryData.id === undefined) {
-            var title = "Welcome";
-            var description = "Hello, Node.js";
-            var template = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Sangho ${title}</title>
-            </head>
-            <body>
-                <h1><a href="/">Web Study</h1>
-                <ul>
-                    <li><a href="/?id=home">home</a></li>
-                    <li><a href="/?id=Portfolio">Portfolio</a></li>
-                    <li><a href="/?id=About">About</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
+            fs.readdir("./data", function (error, filelist) {
+                var title = "Welcome";
+                var description = "Hello, Node.js";
+                var list = templateList(filelist);
+                var template = templateHTML(
+                    title,
+                    list,
+                    `<h2>${title}</h2>${description}`
+                );
+
+                response.writeHead(200);
+                response.end(template);
+            });
         } else {
-            fs.readFile(
-                `data/${queryData.id}`,
-                "utf-8",
-                function (err, description) {
-                    var title = queryData.id;
-                    var template = `
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Sangho ${title}</title>
-                </head>
-                <body>
-                    <h1><a href="/">Web Study</h1>
-                    <ul>
-                        <li><a href="/?id=home">home</a></li>
-                        <li><a href="/?id=Portfolio">Portfolio</a></li>
-                        <li><a href="/?id=About">About</a></li>
-                    </ul>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-            `;
-                    response.writeHead(200);
-                    response.end(template);
-                }
-            );
+            fs.readdir("./data", function (error, filelist) {
+                fs.readFile(
+                    `data/${queryData.id}`,
+                    "utf-8",
+                    function (err, description) {
+                        var title = queryData.id;
+                        var list = templateList(filelist);
+                        var template = templateHTML(
+                            title,
+                            list,
+                            `<h2>${title}</h2>${description}`
+                        );
+                        response.writeHead(200);
+                        response.end(template);
+                    }
+                );
+            });
         }
     } else {
         response.writeHead(404);
