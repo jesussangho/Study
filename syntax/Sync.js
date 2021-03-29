957a90b1b59cb1e9e0445e8ec35b487521335d5c
jesussangho <jesussangho@gmail.com>
2021-03-29 14:21:02 +0900

    fix sync

--- syntax/Sync.js
+++ syntax/Sync.js
@@ -2,17 +2,17 @@ var fs = require("fs");
 
 // readFileSync 동기 , A B C
 
-console.log("A");
-var result = fs.readFileSync("./sample.txt", "utf-8");
-console.log(result);
-console.log("C");
+// console.log("A");
+// var result = fs.readFileSync("syntax/sample.txt", "utf-8");
+// console.log(result);
+// console.log("C");
 
 // readFile 비동기 , A C B
 
-// console.log("A");
-// fs.readFile("./sample.txt", "utf-8", function (err, result) {
-//     console.log(result);
-// });
-// console.log("C");
+console.log("A");
+fs.readFile("syntax/sample.txt", "utf-8", function (err, result) {
+    console.log(result);
+});
+console.log("C");
 
 // A C B 비동기
