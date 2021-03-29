e6d3965380c574cd2a8724bc02d900349cb894dd
jesussangho <jesussangho@gmail.com>
2021-03-29 16:22:46 +0900

    fix main.js

--- main.js
+++ main.js
@@ -2,40 +2,8 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
+var template = require("./lib/template.js");
 // refactoring
-var template = {
-    html: function (title, list, body, control) {
-        return `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sangho ${title}</title>
-        </head>
-        <body>
-            <h1><a href="/">Web Study</h1>
-            ${list}
-            ${control}
-            ${body}
-        </body>
-        </html>
-        `;
-    },
-    list: function (filelist) {
-        var list = "<ul>";
-        var i = 0;
-        while (i < filelist.length) {
-            list =
-                list +
-                `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-        }
-        list = list + "</ul>";
-        return list;
-    },
-};
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
