c35e096c4bc48d1bd04413f3a58330d42c3a6b95
jesussangho <jesussangho@gmail.com>
2021-04-02 23:17:36 +0900

    add update

---
+++ test/update.sql
@@ -0,0 +1,9 @@
+/*
+    DATABASE : test
+    Update
+*/
+
+-- author sungjun update
+UPDATE topic SET author='sungjun', title='MongoDB' WHERE id=2;
+UPDATE topic SET author='sungjun', title='MariaDB' WHERE id=3;
+UPDATE topic SET author='sungjun', title='SQL Server' WHERE id=4;
