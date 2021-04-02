23067a98149e86847b0a65adde7e882c7b4a6007
jesussangho <jesussangho@gmail.com>
2021-04-02 23:17:17 +0900

    add insert

---
+++ test/insert.sql
@@ -0,0 +1,20 @@
+/*
+    DATABASE : test
+    Insert
+    NOW() : 현재시간
+*/
+
+INSERT INTO topic (title, description, created, author, profile)
+VALUES ('MySQL','MySQL is ...', NOW(),'sangho','developer');
+
+INSERT INTO topic (title, description, created, author, profile)
+VALUES ('MongoDB','MongoDB is ...', NOW(),'sangho','developer');
+
+INSERT INTO topic (title, description, created, author, profile)
+VALUES ('MariaDB','MariaDB is ...', NOW(),'sangho','developer');
+
+INSERT INTO topic (title, description, created, author, profile)
+VALUES ('SQL Server','SQL Server is ...', NOW(),'sangho','developer');
+
+INSERT INTO topic (title, description, created, author, profile)
+VALUES ('PostgreSQL','PostgreSQL is ...', NOW(),'sangho','developer');
